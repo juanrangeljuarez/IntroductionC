65feadc22cdac6c66c40f44a982a2f256a96c232
juanrangeljuarez <juanrangel1@mail.weber.edu>
2022-12-04 22:08:15 -0700

    Initial commit

---
+++ src/ImportExample.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+import java.lang.System;
+
+public class ImportExample {
+
+    public static void main(String[] args) {
+        Random r = new Random();
+        System.out.println(r.nextInt(10));
+
+    }
+}
